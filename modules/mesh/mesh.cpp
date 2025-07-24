02d33d15563c99416b8b3e9fa4833ac4725b0e63
Pavel Turapin <paveltrpn@integra-s.com>
2025-07-24 15:27:37 +0400

    update

--- modules/mesh/mesh.cpp
+++ modules/mesh/mesh.cpp
@@ -2,13 +2,18 @@ module;
 
 #include <string>
 #include <format>
+#include <vector>
 
 export module mesh : mesh;
 
 namespace tire {
 
-export struct Mesh final {
-private:
+export template <typename FloatType = double>
+struct Mesh final {
+    std::vector<FloatType> vertecies_{};
+    std::vector<unsigned int> indecies_{};
+    std::vector<FloatType> texcrds_{};
+    std::vector<FloatType> normals_{};
 };
 
 }  // namespace tire
